4552195a1a7b82b78a8e0bdbf728d902cf31979b
selmahidle <69514014+selmahidle@users.noreply.github.com>
2024-04-10 11:34:36 +0200

    Update home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../styles/Home.css';
+import '../styles/Home.css'; // Assuming the CSS is in a file named Home.css
 
 const Home = ({ user }) => {
     const [tasks, setTasks] = useState({
@@ -11,31 +11,29 @@ const Home = ({ user }) => {
     });
 
     return (
-        <>
-            <div className="home">
-                <h1>Hello, {user.name}!</h1>
-                {/* Progress bar */}
-                <div className="progress">
-                    <div className="progress-bar" style={{ width: `${tasks.dailyProgress}%` }}>
-                        {tasks.dailyProgress}%
-                    </div>
-                </div>
-                {/* Task Categories */}
-                <div className="categories">
-                    {Object.entries(tasks).filter(([key,]) => key !== 'dailyProgress').map(([key, task]) => (
-                        <div key={key} className="category">
-                            <h2>{key.charAt(0).toUpperCase() + key.slice(1)}</h2>
-                            <span>{task.new} new tasks</span>
-                            <div className="task-progress">
-                                <div className="task-progress-bar" style={{ width: `${(task.total / 24) * 100}%` }}>
-                                    {task.total}/{24}
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+        <div className="home">
+            <h1>Hello, <br/>{user.name}!</h1>
+            <h2 className="subheader">Daily Tasks</h2>
+            <div className="total-progress">
+                <div className="progress-bar" style={{ width: `${tasks.dailyProgress}%` }}>
+                    {tasks.dailyProgress}%
                 </div>
             </div>
-        </>
+            <h2 className="subheader">Categories</h2>
+            <div className="categories">
+                {Object.entries(tasks).filter(([key,]) => key !== 'dailyProgress').map(([key, task]) => (
+                    <div key={key} className="category">
+                        <h3>{key.charAt(0).toUpperCase() + key.slice(1)}</h3>
+                        <span>{task.new} new tasks</span>
+                        <div className="task-progress">
+                            <div className="task-progress-bar" style={{ width: `${(task.total / 24) * 100}%` }}>
+                                {task.total}/{24}
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 };
 
