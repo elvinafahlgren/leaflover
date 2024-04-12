e8a687fd119744ab55ca1acf99c389c966ece03d
Mariia Tokareva <chu2001108@mail.ru>
2024-04-12 10:00:48 +0200

    updated video reference

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -19,7 +19,7 @@ const Articles = () => {
       id: 2,
       title: 'Introduction to Plant Hormones',
       videos: [
-        { id: 'VIDEO_ID_1', title: 'Introduction to Plant Hormones', name: 'Botanical Growth Insights' }
+        { url: 'https://commons.wikimedia.org/wiki/File:Tutoriel_-_S%27encorder_sur_un_glacier_et_anneaux_de_buste_-_%C3%89cole_de_glace_-2.webm?embedplayer=yes', title: 'Introduction to Plant Hormones', name: 'Botanical Growth Insights' }
       ]
     },
     {
@@ -33,7 +33,7 @@ const Articles = () => {
         { url: 'https://source.unsplash.com/800x600/?green-building', title: 'Eco-Construction' }
       ],
       videos: [
-        { id: 'VIDEO_ID_3', title: 'Urban Farming Innovations', name: 'Innovative Horticultural Techniques' }
+        { url: 'https://commons.wikimedia.org/wiki/File:Tutoriel_-_S%27encorder_sur_un_glacier_et_anneaux_de_buste_-_%C3%89cole_de_glace_-2.webm?embedplayer=yes', title: 'Urban Farming Innovations', name: 'Innovative Horticultural Techniques' }
       ]
     }, 
     {
@@ -47,45 +47,49 @@ const Articles = () => {
   ];
 
   return (
-    <div className="articles-container">
-      {articles.map((article, index) => (
-        <div key={index} className="article darken-on-hover"> {/* Added className here */}
-          <a href={`#${article.id}`} className="article-link">
-            <h2>{article.title}</h2>
-          </a>
-          <p>{article.content}</p>
-          {article.photos && (
-            <div className="photo-carousel">
-              <Carousel>
-                {article.photos.map((photo, photoIndex) => (
-                  <div key={photoIndex}>
-                    <img src={photo.url} alt={photo.title} />
-                    <p className="legend">{photo.title}</p>
+    <div className="articles-page">
+      <h1>ARTICLES</h1>
+      <p>Explore a variety of articles covering topics related to plant growth, urban farming, and gardening.  <br></br> Dive into insightful content and enrich your knowledge to cultivate healthier plants and sustainable environments.</p>
+      <div className="articles-container">
+        {articles.map((article, index) => (
+          <div key={index} className="article darken-on-hover">
+            <a href={`#${article.id}`} className="article-link">
+              <h2>{article.title}</h2>
+            </a>
+            <p>{article.content}</p>
+            {article.photos && (
+              <div className="photo-carousel">
+                <Carousel>
+                  {article.photos.map((photo, photoIndex) => (
+                    <div key={photoIndex}>
+                      <img src={photo.url} alt={photo.title} />
+                      <p className="legend">{photo.title}</p>
+                    </div>
+                  ))}
+                </Carousel>
+              </div>
+            )}
+            {article.videos && (
+              <div className="video-container">
+                <h3>VIDEO TUTORIAL</h3>
+                {article.videos.map((video, videoIndex) => (
+                  <div key={videoIndex} className="video">
+                    <iframe
+                      src={video.url}
+                      width="560"
+                      height="315"
+                      frameBorder="0"
+                      allowFullScreen
+                      title={video.title}
+                    ></iframe>
+                    <p>{video.name}</p>
                   </div>
                 ))}
-              </Carousel>
-            </div>
-          )}
-          {article.videos && (
-            <div className="video-container">
-              <h3>VIDEO TUTORIAL</h3> {/* Added header for videos */}
-              {article.videos.map((video, videoIndex) => (
-                <div key={videoIndex} className="video">
-                  <iframe
-                    width="560"
-                    height="315"
-                    src={`https://www.youtube.com/embed/${video.id}`}
-                    title={video.title}
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                  ></iframe>
-                  <p>{video.name}</p>
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
-      ))}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
